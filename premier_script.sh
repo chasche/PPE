b922204b1cd944972ca1ac44ddc1ae26f948c4a9
chasche <shr.c@live.fr>
2022-12-21 23:05:01 +0100

    test

--- premier_script.sh
+++
@@ -1,8 +0,0 @@
-cd Home/Documents/CoursPluritalS1/Projet encadré 1
-echo "où suis-je?"
-echo “pour l’année 2016” > sortie.txt
-grep “location” 2016*.ann | wc -l >> sortie.txt
-echo “pour l’année 2017” >> sortie.txt
-grep "location" 2017*.ann | wc
-echo "pour l'année 2018" >> sortie.txt
-grep "location" 2018*.ann | wc -l >> sortie.txt
